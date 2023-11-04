881441e51d6824e60e66aaa2066e0212ac436caf
amaz11 <k.m.shaonred@gmail.com>
2023-11-04 11:15:02 +0600

    "image github location change"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,51 +5,50 @@ import Header from '../components/Header'
 import ImageGrid from '../components/ImageGrid'
 
 
-
 const Home = () => {
     const [data, setData] = useState([
         {
             id: 1,
-            image: "../assets/image-1.webp"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-1.webp"
         },
         {
             id: 2,
-            image: "../assets/image-2.webp"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-2.webp"
         },
         {
             id: 3,
-            image: "../assets/image-3.webp"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-3.webp"
         },
         {
             id: 4,
-            image: "../assets/image-4.webp"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-4.webp"
         },
         {
             id: 5,
-            image: "../assets/image-5.webp"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-5.webp"
         }, {
 
             id: 6,
-            image: "../assets/image-6.webp"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-6.webp"
         },
         {
             id: 7,
-            image: "../assets/image-7.webp"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-7.webp"
         },
         {
             id: 8,
-            image: "../assets/image-8.webp"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-8.webp"
         }, {
             id: 9,
-            image: "../assets/image-9.webp"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-9.webp"
         },
         {
             id: 10,
-            image: "../assets/image-10.jpeg"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-10.jpeg"
         },
         {
             id: 11,
-            image: "../assets/image-11.jpeg"
+            image: "https://raw.githubusercontent.com/amaz11/olloy_reactjs/main/src/assets/image-11.jpeg"
         }
     ])
     const [targetId, setTargetId] = useState([])
